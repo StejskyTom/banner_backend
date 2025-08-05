ef582087e5abdda4d8d5609a046b8a18f40146ae
Tomas Stejskal <tomas.stejskal@redbit.cz>
2025-08-05 21:58:53 +0200

    feat: cors and register

--- src/User/Infrastructure/DTO/Request/RegisterRequest.php
+++ src/User/Infrastructure/DTO/Request/RegisterRequest.php
@@ -13,13 +13,4 @@ class RegisterRequest
     #[Assert\NotBlank(message: 'Heslo je povinné')]
     #[Assert\Length(min: 8, minMessage: 'Heslo musí mít alespoň 8 znaků')]
     public string $password;
-
-    public static function fromArray(array $data): self
-    {
-        $request = new self();
-        $request->email = $data['email'] ?? '';
-        $request->password = $data['password'] ?? '';
-
-        return $request;
-    }
 }
